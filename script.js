e837fe2f74a74506955bf90182e738cf9484b886
Malene Ulm <32540888+MaleneMango@users.noreply.github.com>
2018-05-17 12:20:28 +0200

    Sort by category on "Arrangementer" + Fixing checkbox on style guide

---
+++ script.js
@@ -0,0 +1,95 @@
+// Sorting system on "Arrangementer"
+window.onload = function() {
+    document.getElementsByClassName("checkbox").checked = true;
+} // Make sure all checkboxes are checked onload
+
+// show/hide Cat1 "Sport og motion"
+function showCat1() {
+    var y = document.getElementsByClassName("cat1");
+    var i;
+    for (i = 0; i < y.length; i++) { // for-loop
+      if (y[i].style.display === 'none')
+      {y[i].style.display = 'block';}
+      else {
+        y[i].style.display = 'none';
+    }
+  }
+}
+
+// show/hide Cat2 "Fællesspisning"
+function showCat2() {
+    var y = document.getElementsByClassName("cat2");
+    var i;
+    for (i = 0; i < y.length; i++) { // for-loop
+      if (y[i].style.display === 'none')
+      {y[i].style.display = 'block';}
+      else {
+        y[i].style.display = 'none';
+    }
+  }
+}
+
+// show/hide Cat3 "Workshops"
+function showCat3() {
+    var y = document.getElementsByClassName("cat3");
+    var i;
+    for (i = 0; i < y.length; i++) { // for-loop
+      if (y[i].style.display === 'none')
+      {y[i].style.display = 'block';}
+      else {
+        y[i].style.display = 'none';
+    }
+  }
+}
+
+// show/hide Cat4 "Foredrag"
+function showCat4() {
+    var y = document.getElementsByClassName("cat4");
+    var i;
+    for (i = 0; i < y.length; i++) { // for-loop
+      if (y[i].style.display === 'none')
+      {y[i].style.display = 'block';}
+      else {
+        y[i].style.display = 'none';
+    }
+  }
+}
+
+// show/hide Cat5 "Fest og højtid"
+function showCat5() {
+    var y = document.getElementsByClassName("cat5");
+    var i;
+    for (i = 0; i < y.length; i++) { // for-loop
+      if (y[i].style.display === 'none')
+      {y[i].style.display = 'block';}
+      else {
+        y[i].style.display = 'none';
+    }
+  }
+}
+
+// show/hide Cat6 "Film"
+function showCat6() {
+    var y = document.getElementsByClassName("cat6");
+    var i;
+    for (i = 0; i < y.length; i++) { // for-loop
+      if (y[i].style.display === 'none')
+      {y[i].style.display = 'block';}
+      else {
+        y[i].style.display = 'none';
+    }
+  }
+}
+
+// show/hide Cat7 "Musik og teater"
+function showCat7() {
+    var y = document.getElementsByClassName("cat7");
+    var i;
+    for (i = 0; i < y.length; i++) { // for-loop
+      if (y[i].style.display === 'none')
+      {y[i].style.display = 'block';}
+      else {
+        y[i].style.display = 'none';
+    }
+  }
+}
